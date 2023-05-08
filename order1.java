748983778d00d644624a44adad19d25d97118831
rhuchasant <100152479+rhuchasant@users.noreply.github.com>
2023-05-08 17:15:20 +0530

    Create order1.java

---
+++ order1.java
@@ -0,0 +1,67 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/order1")
+public class order1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+		
+		int pid=Integer.parseInt(request.getParameter("Pid"));
+		int qty=Integer.parseInt(request.getParameter("QTY"));
+		RequestDispatcher dispatcher=null;
+		Connection con=null;
+		
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			 con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mini","root","Rhucha2301$");
+			PreparedStatement pst=con.prepareStatement("Insert into order_product (PID,Qty) values (?,?)");
+			pst.setInt(1, pid);
+			pst.setInt(2, qty);
+			
+			int rowCount=pst.executeUpdate();
+			dispatcher=request.getRequestDispatcher("Menu1.jsp");
+			if(rowCount>0) {
+				request.setAttribute("status", "success");
+			}
+			else {
+				request.setAttribute("status", "failed");
+			}
+			
+			dispatcher.forward(request, response);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+		
+		
+		
+	}
+
+
